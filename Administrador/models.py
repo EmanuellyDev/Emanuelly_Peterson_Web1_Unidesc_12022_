aa3c9ec284e04400ac550a38253f0d0170737e44
Emanuelly Silva <81655777+EmanuellyDev@users.noreply.github.com>
2022-06-22 21:36:18 -0300

    Add files via upload

---
+++ Administrador/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Administrador(models.Model):
+
+    def calcular_Salario(Double):
+        comissao = int(input('inclua seu agendamento: '))
+        idCorretor = input('inclua seu agendamento: ')
+
+        calcular = comissao + idCorretor
+        return calcular
